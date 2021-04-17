8a5a9559fbcc1f01085d12ed4dfeda61321484c7
Aldán Creo <20495460+ACMCMC@users.noreply.github.com>
2021-04-17 12:09:03 +0200

    Update append.lisp

--- append.lisp
+++ append.lisp
@@ -1,9 +1,9 @@
-(defun cadr (x) (car (cdr x)))
+(defun cadr (x) (car (cdr x))) ; realmente ya viene definida
 (defun append (lst x) (if
-                        (eq (cadr lst) nil)
-                        (cons (car lst) (cons x nil))
-                        (cons (car lst) (append (cdr lst) x))))
-(setq x '(1 2 3))
-(setq x2 (append (list 1 2 3) 5))
+                        (eq (cadr lst) nil) ; si estamos al final de la lista...
+                        (cons (car lst) (cons x nil)) ; devolvemos una nueva celda cons que sea (ultimo_elem_de_lst x)
+                        (cons (car lst) (append (cdr lst) x)))) ; devolvemos (elemento_actual_de_lst append_del_siguiente)
+(setq x (list 1 2 3))
+(setq x2 (append x 5))
 (print x)
 (print x2)
